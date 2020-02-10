f71100a4a3251fb5f3300ce972638f92b427e2f9
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-02-10 14:54:16 -0600

    printing decoded log

--- subprocess_utils.py
+++ subprocess_utils.py
@@ -20,18 +20,39 @@ def print_cmd_if_needed(cmd, print_cmd):
 '''
 ''' VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV '''
 
+# if no output, returns none
+# if outputs one line, returns string
+# if outputs multiple lines, returns list
 # try to use this first, if you get an error like below, try run_cmd_call()
     #     for arg in seq:
     # TypeError: 'bool' object is not iterable
-def run_cmd_popen(cmd, print_output = False, print_cmd = False, shell = False):
+def run_cmd_popen(cmd, print_output = False, print_cmd = False, shell = False, decode = False):
     print_cmd_if_needed(cmd, print_cmd)
+    
+    output_line_l = []
         
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1, shell = shell)
     for line in iter(p.stdout.readline, b''):
+        
+        if decode:
+            line = line.decode("utf-8") 
+        
+        output_line_l.append(line)
         if print_output:
             print (line)
     p.stdout.close()
     p.wait()
+    
+    print(len(output_line_l))#````````````````````````````````````````````````````````````````````````````
+    
+    if len(output_line_l) == 0:
+        return None
+    elif len(output_line_l) == 1:
+        print('should return ', output_line_l[0])#`````````````````````````````````````````````````````````
+        return output_line_l[0]
+        return output_line_l[0]
+    else:
+        return output_line_l
 
 # try to only use this if run_cmd_popen does not work
 def run_cmd_call(cmd, print_cmd = False, shell = False):
@@ -57,9 +78,11 @@ def fatal_error(cmd):
     
     
     
-    
-    
-    
+#     
+# if __name__ == "__main__":
+#     from submodules.git_tools import Git_Commit
+# 
+#     Git_Commit.main()
     
     
     
